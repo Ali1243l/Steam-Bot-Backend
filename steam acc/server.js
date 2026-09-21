606289e67f5a91d2b8cb75e54db5a99e9c4e59bb
Ali1243l <114656438+Ali1243l@users.noreply.github.com>
2026-09-21 03:50:35 +0300

    Add files via upload

--- steam acc/server.js
+++ steam acc/server.js
@@ -36,7 +36,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 async function bootstrap() {
   const app = express();
